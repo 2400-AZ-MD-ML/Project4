2baa4c6d06f98176725f411f013476f93e9b7a85
AlvanZ <alvanzhuang625@gmail.com>
2022-04-18 09:20:11 -0700

    added main method class, but needs comments/and finish main method

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,9 +1,6 @@
 package Project4;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+
 /**
    A class that implements the ADT maxheap by using an array.
  
@@ -158,31 +155,5 @@ private void ensureCapacity(){
       heap = Arrays.copyOf(heap,newLength);
    }
 }
-public static void main(String[] args) throws FileNotFoundException{
 
-//relative path 
-File file = new File("Project4\\data_sorted.txt");
-Scanner scan = new Scanner(file);
-Integer[] heapData = new Integer[100];
-MaxHeap<Integer> iter = new MaxHeap<>(101);
-int i =0;
-while(scan.hasNextInt()){
-int next = scan.nextInt();
-System.out.print(next + " ");
-iter.add(next);
-heapData[i++] = next;
-}
-System.out.println();
-// for(int index=1; index<=10; index++){
-//    System.out.print(iter.get(index) + " ");
-// }
-MaxHeap<Integer> smart = new MaxHeap<>(heapData);
-for(int index = 1; index<=10; index++){
-   System.out.print(smart.get(index) + " ");
-}
-System.out.println();
-System.out.println(iter.getSwaps());
-
-scan.close();
-}
 } // end MaxHeap
