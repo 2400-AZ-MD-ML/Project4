2848e263327096ceafd5003cf0de51878f0e6635
mmlado-cpp <98682021+mmlado-cpp@users.noreply.github.com>
2022-04-18 16:20:36 -0700

    updated driver class. need to add file writer

--- MaxHeapDriver.java
+++ MaxHeapDriver.java
@@ -2,29 +2,39 @@ package Project4;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+
 public class MaxHeapDriver {
     public static void main(String[] args) throws FileNotFoundException{
 
         //relative path 
-        File file = new File("Project4\\data_sorted.txt");
+        File file = new File("data_sorted.txt"); //open file to read from and create heap
         Scanner scan = new Scanner(file);
         Integer[] heapData = new Integer[100];
         MaxHeap<Integer> iter = new MaxHeap<>(101);
-        int i =0;
+
+        // loop to iterate & print through integers in data_sorted.txt
+        int i = 0;
         while(scan.hasNextInt()){
-        int next = scan.nextInt();
-        System.out.print(next + " ");
-        iter.add(next);
-        heapData[i++] = next;
+            int next = scan.nextInt();
+            System.out.print(next + " ");
+            iter.add(next);
+            heapData[i++] = next;
         }
+        
         System.out.println();
-        // for(int index=1; index<=10; index++){
-        //    System.out.print(iter.get(index) + " ");
-        // }
+
+        // for loop below used for writing first 10 integers to new file
+        /*
+        for(int index=1; index<=10; index++){
+            System.out.print(iter.get(index) + " ");
+        }
+        */
+
         MaxHeap<Integer> smart = new MaxHeap<>(heapData);
         for(int index = 1; index<=10; index++){
            System.out.print(smart.get(index) + " ");
         }
+
         System.out.println();
         System.out.println(iter.getSwaps());
         
